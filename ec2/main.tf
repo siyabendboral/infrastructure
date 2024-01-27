f526cf1f47440c3d859a654e1141afcc390fd159
siyabendboral <148371703+siyabendboral@users.noreply.github.com>
2024-01-27 12:06:31 -0600

    added sec grp config

--- ec2/main.tf
+++ ec2/main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  subnet_id = data.terraform_remote_state.this.outputs.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
